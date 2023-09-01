3459aa6ee9b7c14fe7fb380dcbc83c8d3fae3c60
Kulvir Singh <ksingh11_be20@thapar.edu>
2023-09-02 03:32:42 +0530

    Clerk Auth Added

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { ClerkProvider, SignedIn, UserButton } from '@clerk/nextjs';
+import { Open_Sans } from 'next/font/google';
+import { Metadata } from 'next';
+import './globals.css';
+
+const OpenSans = Open_Sans({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Dicsword',
+  description:
+    'A Discord like application for those homie calls and those homie gaming nights.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <ClerkProvider>
+        <body className={`${OpenSans.className} min-h-screen flex flex-col`}>
+          <header className='flex items-center h-20 gap-4 px-4'>
+            <SignedIn>
+              <UserButton afterSignOutUrl='/' />
+            </SignedIn>
+          </header>
+          <main>{children}</main>
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='bg-red-300'>
+      <h1 className='h-48 text-4xl'>Dicsword application</h1>
+    </div>
+  );
+}
