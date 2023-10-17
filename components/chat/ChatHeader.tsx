54bfb3259ce4d8225252069ebfc674c59cce7f6f
Kulvir Singh <ksingh11_be20@thapar.edu>
2023-10-17 16:57:57 +0530

    conversation setup done

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,5 +1,6 @@
 import { Hash } from 'lucide-react';
 import MobileToggle from '@/components/MobileToggle';
+import UserAvatar from '@/components/UserAvatar';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -14,6 +15,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
       <MobileToggle serverId={serverId} />
       {type === 'channel' && (
         <Hash className="w-5 h-5 mr-2 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === 'conversation' && (
+        <UserAvatar src={imageUrl} className="w-8 h-8 mr-2" />
       )}
       <p className="font-semibold text-black text-md dark:text-white">{name}</p>
     </div>
