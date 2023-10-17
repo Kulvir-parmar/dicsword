d42e35c973749cd73a03febf02014d6eadc32c49
Kulvir Singh <ksingh11_be20@thapar.edu>
2023-10-17 12:46:39 +0530

    Channel ID page created

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,46 @@
-function ServerIDPage() {
-  return <div>This is the server ID Page</div>;
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+import { CurrentProfile } from '@/lib/CurrentProfile';
+import { db } from '@/lib/db';
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
 }
+
+const ServerIDPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await CurrentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'general',
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== 'general') return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
+};
 
 export default ServerIDPage;
