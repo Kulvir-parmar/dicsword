115c386d0476bbacab0428676af9a60ee865a0f7
Kulvir Singh <ksingh11_be20@thapar.edu>
2023-10-21 11:44:52 +0530

    Minor issues fixed

--- types.ts
+++ types.ts
@@ -3,7 +3,7 @@ import { NextApiResponse } from 'next';
 import { Server as SocketIOServer } from 'socket.io';
 import { Server, Member, Profile } from '@prisma/client';
 
-export type ServerWithMembersWithProfile = Server & {
+export type ServerWithMemberWithProfile = Server & {
   members: (Member & { profile: Profile })[];
 };
 
